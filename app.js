5792efaa70908313cc3da56091ec227e768c37f3
ispykenny <kenny.krosky@gmail.com>
2020-02-13 15:37:26 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cheerio = require('cheerio')
+let PORT = 4000 || PORT;
+let latestReport = "";
+
+app.use('/', (req, res, error) => {
+  res.json(latestReport);
+})
+
+
+axios.get("https://status.dexcom.com/")
+  .then((response) => {
+    const $ = cheerio.load(response.data);
+    const report = $('.status-day');
+    latestReport = {
+      reported: $(report[0]).find('p').text()
+    };
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
+
+
+app.listen(PORT, () => console.log(`listening to port ${PORT}`))
