aa238f20dd6e8a584141aea425513203d7dad391
ispykenny <kenny.krosky@gmail.com>
2020-02-13 16:21:05 -0800

    added date

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const cheerio = require('cheerio')
 let PORT = process.env.PORT || 4000;
 let latestReport = "";
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use('/', (req, res, error) => {
   axios.get("https://status.dexcom.com/")
   .then((response) => {
     const $ = cheerio.load(response.data);
     const report = $('.status-day');
     latestReport = {
-      reported: $(report[0]).find('p').text()
+      reported: $(report[0]).find('p').text(),
+      date: $(report[0]).find('.date').text()
     };
     res.json(latestReport);
   })
