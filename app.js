c66cfddcfc0a0e3921277a4e95d2dd751b15447f
ispykenny <kenny.krosky@gmail.com>
2020-02-15 15:39:21 -0800

    tracking more operations

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
 const moment = require('moment');
+const url = require('url');
 let PORT = process.env.PORT || 4000;
-let latestReport = "";
+let allReports = [];
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -14,22 +15,37 @@ app.use((req, res, next) => {
 
 
 app.use('/', (req, res, error) => {
-  axios.get("https://status.dexcom.com/")
+  axios.get('https://status.dexcom.com/')
   .then((response) => {
     const $ = cheerio.load(response.data);
-    const report = $('.status-day');
-    latestReport = {
-      reported: $(report[0]).find('p').text(),
-      postedTime: moment().subtract(8 , 'hour').format('MMMM Do YYYY, h:mm:ss a'),
-      date: $(report[0]).find('.date').text()
-    };
-    res.json(latestReport);
+    const report = $('.component-inner-container');
+    let localUrl = 'http://localhost:4000/';
+    let checkUrl = url.parse(localUrl, true).hostname
+    let timeStamp = '';
+    if(checkUrl === "localhost") {
+      timeStamp =  moment().format('MMMM Do YYYY, h:mm:ss a');
+    } else {
+      timeStamp =  moment().subtract(8 , 'hour').format('MMMM Do YYYY, h:mm:ss a');
+    }
+    let report_item = '';
+    $(report).each(function() {
+      let $reportType = $(this).find('.name').text().trim();
+      let $reportStatus = $(this).find('.component-status').text().trim();
+      report_item = {
+        report_type: $reportType,
+        report_status: $reportStatus,
+        time: timeStamp
+      }
+      allReports.push(report_item)
+    })
+
+    res.json(allReports)
+    
   })
   .catch((error) => {
     console.log(error)
   })
-})
-
+});
 
 
 
